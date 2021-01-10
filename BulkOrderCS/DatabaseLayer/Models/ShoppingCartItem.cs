4b5cafe75ce0b69007e02cb681152354f9fc7dde
Milica Kovacevic <206-2018@ftn.edu.rs>
2021-01-10 21:33:24 +0100

    Kreirane model klase Book, Client, ShoppingCart, ShoppingCartItem, ShoppingCartItemForList

---
+++ BulkOrderCS/DatabaseLayer/Models/ShoppingCartItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLayer.Models
+{
+    public class ShoppingCartItem
+    {
+        public ShoppingCartItem(int id, int quantity, int shoppingCart, int isbn)
+        {
+            this.Id = id;
+            this.Quantity = quantity;
+            this.shoppingCart = shoppingCart;
+            this.Isbn = isbn;
+        }
+
+        private int id;
+        private int quantity;
+        private int shoppingCart;
+        private int isbn;
+
+        public int Id { get => id; set => id = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+        public int ShoppingCart { get => shoppingCart; set => shoppingCart = value; }
+        public int Isbn { get => isbn; set => isbn = value; }
+    }
+}
---
+++ BulkOrderCS/DatabaseLayer/Models/ShoppingCartItemForList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLayer.Models
+{
+    // {0} - cover; {1} - title; {2} - author; {3} -  i; {4} - quantity; {5} - price; {6} - price * quantity; {7} - item id; {8} - isbn
+    public class ShoppingCartItemForList
+    {
+
+
+        private string cover;
+        private string title;
+        private string author;
+        private int quantity;
+        private double price;
+        private int itemId;
+        private int isbn;
+
+        public ShoppingCartItemForList(string cover, string title, string author, int quantity, double price, int itemId, int isbn)
+        {
+            this.cover = cover;
+            this.title = title;
+            this.author = author;
+            this.quantity = quantity;
+            this.price = price;
+            this.itemId = itemId;
+            this.isbn = isbn;
+        }
+
+        public string Cover { get => cover; set => cover = value; }
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+        public double Price { get => price; set => price = value; }
+        public int ItemId { get => itemId; set => itemId = value; }
+        public int Isbn { get => isbn; set => isbn = value; }
+    }
+}
