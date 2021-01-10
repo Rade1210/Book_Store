8e225e0bc58ef5a5ba2ee4671406906f96c0d321
Rade Petrovic <100-2018@ftn.edu.rs>
2021-01-10 22:03:16 +0100

    Kreirana klasa ShoppingCartItemsBusiness koja vraca sve podatke iz shopping cart-ova i vrsi IUD operacije

---
+++ BulkOrderCS/BusinessLogicLayer/ShoppingCartItemsBusiness.cs
@@ -0,0 +1,48 @@
+﻿using DatabaseLayer;
+using DatabaseLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class ShoppingCartItemsBusiness
+    {
+        private ShoppingCartItemsRepository shoppingCartItemsRepository;
+        public ShoppingCartItemsBusiness()
+        {
+            this.shoppingCartItemsRepository = new ShoppingCartItemsRepository();
+        }
+        public List<ShoppingCartItem> GetAllShoppingCartItems()
+        {
+            return this.shoppingCartItemsRepository.GetAllShoppingCartItems();
+        }
+        public List<ShoppingCartItemForList> itemsForList(int id)
+        {
+            return this.shoppingCartItemsRepository.itemsForList(id);
+        }
+        public string InsertShoppingCartItem(ShoppingCartItem shoppingCartItem)
+        {
+            if (shoppingCartItemsRepository.InsertShoppingCartItem(shoppingCartItem) != 0)
+                return "Shopping cart item successfully inserted into the database!";
+            else
+                return "Shopping cart item was not inserted into the database!";
+        }
+        public string DeleteShoppingCartItem(int id)
+        {
+            if (shoppingCartItemsRepository.DeleteShoppingCartItem(id) != 0)
+                return "Shopping cart item successfully deleted!";
+            else
+                return "Shopping cart item was not deleted!";
+        }
+        public string UpdateShoppingCartItem(ShoppingCartItem shoppingCartItem)
+        {
+            if (shoppingCartItemsRepository.UpdateShoppingCartItem(shoppingCartItem) != 0)
+                return "Shopping cart item successfully updated!";
+            else
+                return "Shopping cart item was not updated!";
+        }
+    }
+}
